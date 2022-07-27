25a1695c57d3bcfc5730553d2054718d65ef555e
YeoYongZeng <yeoyongzeng@gmail.com>
2022-07-27 20:32:49 +0800

    Add checker

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+string input = "1+(2.1)+1^0.5+2^2+2-*/";
+char[] inputStrToArray = input.ToCharArray();
+
+foreach(char tempChar in inputStrToArray)
+{
+    bool isOk = false;
+    //Console.WriteLine($"in foreach {tempChar}");
+    if (tempChar == '+')
+    {
+        isOk = true;
+        continue;
+    }
+    if (tempChar == '-')
+    {
+        isOk = true;
+        continue;
+    }
+}
+Console.WriteLine("Hello, World!");
---
+++ Calculator/StringChecker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Calculator
+{
+    public class StringChecker
+    {
+        public bool Checker_IfOneByOne(string input)
+        {
+            foreach(char chr in input.ToCharArray())
+            {
+                if (chr == '+') continue;
+                if (chr == '-') continue;
+                if (chr == '*') continue;
+                if (chr == '/') continue;
+                if (chr == '^') continue;
+                if (chr == '(') continue;
+                if (chr == ')') continue;
+                if (chr >= '0' && chr <= '9') continue;
+
+                return false;
+            }
+            return true;
+        }
+
+        public bool Checker_Substring(string input)
+        {
+            for(int i=0; i< input.Length; i++)
+            {
+                string temp = input.Substring(i, 1);
+                if (temp == "+") continue;
+                if (temp == "-") continue;
+                if (temp == "*") continue;
+                if (temp == "/") continue;
+                if (temp == "^") continue;
+                if (temp == "(") continue;
+                if (temp == ")") continue;
+                if (temp == "0") continue;
+                if (temp == "1") continue;
+                if (temp == "2") continue;
+                if (temp == "3") continue;
+                if (temp == "4") continue;
+                if (temp == "5") continue;
+                if (temp == "6") continue;
+                if (temp == "7") continue;
+                if (temp == "8") continue;
+                if (temp == "9") continue;
+                return false;
+            }
+            return true;
+        }
+
+        public bool Checker_Contains(string input)
+        {
+            string template = "1234567890/*+-^()";
+            foreach (char chr in input.ToCharArray())
+            {
+                if (template.Contains(chr))
+                    continue;
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
